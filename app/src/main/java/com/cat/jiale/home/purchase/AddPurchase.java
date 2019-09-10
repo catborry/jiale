5babb9b4cae55eb2fdb5ff7b12207c455c85e67e
Administrator <2591981044@qq.com>
2019-09-10 15:04:00 +0800

    测试提交

--- app/src/main/java/com/cat/jiale/home/purchase/AddPurchase.java
+++ app/src/main/java/com/cat/jiale/home/purchase/AddPurchase.java
@@ -3,6 +3,7 @@ package com.cat.jiale.home.purchase;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import com.cat.jiale.R;
 
@@ -12,5 +13,6 @@ public class AddPurchase extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_purchase);
+        Log.e("s","s");
     }
 }
