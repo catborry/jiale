4800ddb110019f87f21693fcfab7824635c39313
Administrator <2591981044@qq.com>
2019-09-11 15:00:26 +0800

    照相功能的实现

--- app/src/main/java/com/cat/jiale/home/purchase/AddPurchase.java
+++ app/src/main/java/com/cat/jiale/home/purchase/AddPurchase.java
@@ -1,17 +1,65 @@
 package com.cat.jiale.home.purchase;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.FileProvider;
 
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 import com.cat.jiale.R;
 
+import java.io.File;
+import java.io.IOException;
+
 public class AddPurchase extends AppCompatActivity {
+    private Button button;
+    private ImageView imageView;
+    private Uri imageUri;
+    public static File tempFile;
+    public static final int TAKE_PHOTO = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_purchase);
+        imageView=findViewById(R.id.imgnow);
+        button=findViewById(R.id.img);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent it = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(it,Activity.DEFAULT_KEYS_DIALER);
+
+            }
+        });
+    }
+//    @Override
+//    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+//        if(requestCode == Activity.RESULT_OK){
+//            Bundle bundle = data.getExtras();
+//            Bitmap bitmap = (Bitmap) bundle.get("data");
+//            img_show.setImageBitmap(bitmap);
+//        }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == Activity.RESULT_OK){
+            Bundle bundle = data.getExtras();
+            Bitmap bitmap = (Bitmap) bundle.get("data");
+            imageView.setImageBitmap(bitmap);
+        }
     }
 }
