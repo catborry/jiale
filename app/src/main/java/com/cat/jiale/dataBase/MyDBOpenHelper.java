a8d2872a416e38e6cb9d5b0901fefd7cc2c766a5
jiale <2591981044@qq.com>
2019-10-15 16:22:26 +0800

    数据库处理

--- app/src/main/java/com/cat/jiale/dataBase/MyDBOpenHelper.java
+++ app/src/main/java/com/cat/jiale/dataBase/MyDBOpenHelper.java
@@ -1,18 +1,21 @@
-package com.cat.jiale.sql;
+package com.cat.jiale.dataBase;
 
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
-import android.media.session.MediaSession;
+import android.util.Log;
 import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 
 public class MyDBOpenHelper extends SQLiteOpenHelper {
-    Context context;
-    public MyDBOpenHelper(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
-        super(context, "jiale.db", null, 1);
-        this.context=context;
+    //数据库名
+    private static final String DATABASE_NAME="jiale.db";
+    //版本号
+    private static int DATABASE_VERSION=1;
+
+    public MyDBOpenHelper(@Nullable Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
         Toast.makeText(context,"数据库创建",Toast.LENGTH_LONG).show();
 
     }
@@ -21,7 +24,8 @@ public class MyDBOpenHelper extends SQLiteOpenHelper {
     //数据库第一次创建时被调用
     @Override
     public void onCreate(SQLiteDatabase sqLiteDatabase) {
-        Toast.makeText(context,"数据库创建",Toast.LENGTH_LONG).show();
+        sqLiteDatabase.execSQL("create table Purchase(id integer primary key autoincrement not null,name text)");
+        Log.d("数据库创建","success");
     }
     //数据库更新的时候调用
     @Override
---
+++ app/src/main/java/com/cat/jiale/dataBase/Purchase/PurchaseCRUD.java
@@ -0,0 +1,5 @@
+package com.cat.jiale.dataBase.Purchase;
+
+//供货商操作类
+public class PurchaseCRUD {
+}
